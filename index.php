b808d95d920df5753919d6a890334da8fc7d8435
guzenkov <guzenkovsn@gmail.com>
2024-05-02 22:05:50 +0300

    typos

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-Hello Beatiful people!
-A sample PHP program welcomes You.
-Below is a result of phponfo function:
+Hello Beatiful people! <p\> 
+A sample PHP program welcomes You.<p\>
+Below is a result of phpinfo function:
 <?php phpinfo(); ?>
